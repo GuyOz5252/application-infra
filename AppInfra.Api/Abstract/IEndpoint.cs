438e532478105538d0760ef022ac03a2a36fc09a
Guy Oz <109523947+GuyOz5252@users.noreply.github.com>
2026-04-22 22:12:00 +0300

    feat: added api package

---
+++ AppInfra.Api/Abstract/IEndpoint.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Routing;
+
+namespace AppInfra.Api.Abstract;
+
+public interface IEndpoint
+{
+    void MapEndpoint(IEndpointRouteBuilder app);
+}
---
+++ AppInfra.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using AppInfra.Api.Abstract;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace AppInfra.Api.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddEndpoints(this IServiceCollection services, Assembly assembly)
+    {
+        var serviceDescriptors = assembly
+            .DefinedTypes
+            .Where(type => type is { IsAbstract: false, IsInterface: false } &&
+                           type.IsAssignableTo(typeof(IEndpoint)))
+            .Select(type => ServiceDescriptor.Transient(typeof(IEndpoint), type))
+            .ToArray();
+
+        services.TryAddEnumerable(serviceDescriptors);
+    }
+}
